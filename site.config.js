790d80041176a7e72e87be604595d1807a8ff4e2
Nugi9 <james123@hanmail.net>
2024-09-15 01:12:16 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "NUGI's Blog",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
-    bio: "I develop everything using node.",
+    bio: "For a better tomorrow than yesterday",
     email: "inwook789@skku.edu",
     linkedin: "인욱-태-3631392b4",
     github: "",
