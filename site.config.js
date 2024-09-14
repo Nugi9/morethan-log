97f380da430eb57d6d4adc5fca58c26afc667343
Nugi9 <james123@hanmail.net>
2024-09-15 00:07:54 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "NUGI",
+    title: "NUGI-log",
     description: "welcome!",
     scheme: "system", // 'light' | 'dark' | 'system'
   },
